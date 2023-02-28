aeaad00e53dc9d2856c2e0a5a4f40675798b7ca4
Rifqi Fadhil <rifqife@gmail.com>
2023-02-28 10:54:51 +0700

    Adding login template

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import "react-datepicker/dist/react-datepicker.css";
 
+import { store } from "@/store";
+
+import { Provider } from "react-redux";
+
 export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<Provider store={store}>
+			<Component {...pageProps} />
+		</Provider>
+	);
 }
---
+++ src/pages/login/index.js
@@ -0,0 +1,103 @@
+import React, { useEffect } from "react";
+import { Card, TextInput, Button, Checkbox, Label } from "flowbite-react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useRouter } from "next/router";
+import { setCookie, hasCookie, getCookie, deleteCookie } from "cookies-next";
+
+const schema = yup.object().shape({
+	username: yup.string().required("Silakan isi username Anda."),
+	password: yup.string().required("Silakan isi password Anda."),
+});
+
+export default function index() {
+	const router = useRouter();
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		setValue,
+	} = useForm({
+		defaultValues: {
+			username: "",
+			password: "",
+			remember_me: false,
+		},
+		resolver: yupResolver(schema),
+	});
+
+	useEffect(() => {
+		if (hasCookie("remember_me")) {
+			setValue("username", getCookie("username"));
+			setValue("remember_me", getCookie("remember_me"));
+		}
+	}, []);
+
+	async function onSubmit(data) {
+		console.log(data);
+
+		try {
+			if (data.remember_me) {
+				setCookie("username", data.username);
+				setCookie("remember_me", data.remember_me);
+			} else {
+				deleteCookie("username");
+				deleteCookie("remember_me");
+			}
+
+			router.push("/admin/dashboard");
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	return (
+		<div className="h-screen flex items-center">
+			<div className="w-1/3 mx-auto">
+				<Card>
+					<h1 className="text-center text-2xl">Login</h1>
+					<form
+						className="flex flex-col gap-4"
+						onSubmit={handleSubmit(onSubmit)}>
+						<div>
+							<div className="mb-2 block">
+								<Label htmlFor="username" value="Username" />
+							</div>
+							<TextInput
+								id="username"
+								type="text"
+								{...register("username")}
+							/>
+							<small className="text-red-500">
+								{errors.username?.message}
+							</small>
+						</div>
+						<div>
+							<div className="mb-2 block">
+								<Label htmlFor="password" value="Password" />
+							</div>
+							<TextInput
+								id="password"
+								type="password"
+								{...register("password")}
+							/>
+							<small className="text-red-500">
+								{errors.password?.message}
+							</small>
+						</div>
+						<div className="flex items-center gap-2">
+							<Checkbox
+								id="remember_me"
+								{...register("remember_me")}
+							/>
+							<Label htmlFor="remember_me">Remember me</Label>
+						</div>
+						<Button type="submit">Login</Button>
+					</form>
+				</Card>
+			</div>
+		</div>
+	);
+}
