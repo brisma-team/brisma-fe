442fab393265312d50c99abe9e3fd4ca3a715b66
dandy <vitrio@noxus.co.id>
2023-06-21 22:09:22 +0700

    [Dandy] Update code at component Card.js and file tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,22 @@ module.exports = {
         primary: "#3C64B1", // Ganti dengan warna primer yang diinginkan
         secondary: "#00ff00", // Ganti dengan warna sekunder yang diinginkan
         abu: "#FAFBFC", // Ganti dengan warna sekunder yang diinginkan
-        // Tambahkan skema warna lain sesuai kebutuhan Anda
+        atlasian: {
+          dark: "#172B4D",
+          purple: "#5E4DB2",
+          blue: {
+            dark: "#09326C",
+            light: "#0C66E4",
+          },
+          red: "#CA3521",
+          yellow: "#E2B203",
+          green: "#1F845A",
+          gray: {
+            dark: "#97A0AF",
+            light: "#172B4D",
+          },
+        },
+        // Tambahkan skemad warna lain sesuai kebutuhan Anda
       },
     },
   },
