9194c9c8e926f05177b0fde0d6b7bdc764bb80d6
dandy <vitrio@noxus.co.id>
2023-06-21 18:09:48 +0700

    [Dandy] Update code at component card and add some configuration at file tailwind.config.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,21 @@
-import React from 'react'
-import { N800 } from '@atlaskit/theme/colors';
-import { token } from '@atlaskit/tokens';
+import React from "react";
+import { N800 } from "@atlaskit/theme/colors";
+import { token } from "@atlaskit/tokens";
 
-function Card({children}) {
+export default function Card({ children }) {
   return (
     <div
-        className="p-6"
-        style={{
-            color: token('color.text', N800),
-            backgroundColor: token('elevation.surface.overlay', '#fff'),
-            boxShadow: token(
-                'elevation.shadow.overlay',
-                '0px 4px 8px rgba(9, 30, 66, 0.25), 0px 0px 1px rgba(9, 30, 66, 0.31)',
-            ),
-            borderRadius: 4,
-            // margin: `${token('space.200', '16px')} auto`,
-        }}
-  >
+      className="py-2 w-auto rounded flex flex-col items-center"
+      style={{
+        color: token("color.text", N800),
+        backgroundColor: token("elevation.surface.overlay", "#fff"),
+        boxShadow: token(
+          "elevation.shadow.overlay",
+          "0px 4px 8px rgba(9, 30, 66, 0.25), 0px 0px 1px rgba(9, 30, 66, 0.31)"
+        ),
+      }}
+    >
       {children}
     </div>
-  )
+  );
 }
-
-export default Card
