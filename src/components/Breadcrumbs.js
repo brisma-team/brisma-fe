15511105bffb96f948534657287adac91114efec
dandy <vitrio@noxus.co.id>
2023-06-23 13:26:24 +0700

    [Dandy] Install some dependencies, create index file for export icon components, add component breadcrums and update minor code at file CustomDatatable.js

---
+++ src/components/Breadcrumbs.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+// Format for Breadcrums using object inside array
+// example = [
+//     { name: "Menu", path: "/dashboard" },
+//     { name: "PAT", path: "/pat/projects" },
+//     { name: "Overview", path: "/pat/projects/123" },
+//   ];
+
+export default function Breadcrumbs(data) {
+  return (
+    data.length && (
+      <div className="justify-start mb-6 flex text-atlasian-gray-dark">
+        {data.map((v, i) => {
+          return (
+            <div className="text-sm mx-0 flex" key={i}>
+              {i >= 1 && <div className="mx-1">/</div>}
+              <Link className="no-underline hover:no-underline" href={v.path}>
+                {v.name}
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    )
+  );
+}
