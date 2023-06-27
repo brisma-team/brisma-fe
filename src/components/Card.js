eb4578221d7a637adc449fdf49988b1e182abee7
jieeevest <spearheadimz@gmail.com>
2023-06-27 13:21:03 +0700

    [Aji] Add Card Landing

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ const Card = ({ children }) => {
       className="py-2 w-auto rounded flex flex-col items-center"
       style={{
         color: token("color.text", N800),
-        backgroundColor: token("elevation.surface.overlay", "#fff"),
+        // backgroundColor: token("elevation.surface.overlay", "#fff"),
         borderRadius: "10px",
         boxShadow: "0px 0px 4px 0px rgba(0, 0, 0, 0.25)",
       }}
