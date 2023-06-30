4a57a5f2f2586dd052aed7dcd7e60aa5427d94dd
jieeevest <vitrio@noxus.co.id>
2023-06-30 16:45:27 +0700

    [Aji] Fix: Dashboard Chart

--- src/components/molecules/dashboard/BarChart.js
+++ src/components/molecules/dashboard/BarChart.js
@@ -8,140 +8,142 @@ const BarChart = () => {
 
   return (
     <Card>
-      <h1 className="text-xl font-bold">Bar Chart</h1>
-      <div className="h-96">
-        {dashboardIsLoading && (
-          <div className="w-full h-full items-center justify-center flex">
-            <Loader />
-          </div>
-        )}
-        {dashboard && (
-          <AutoSizer>
-            {({ height, width }) => (
-              <Bar
-                height={height}
-                width={width}
-                data={dashboard.data}
-                keys={[
-                  "hot dog",
-                  "burger",
-                  "sandwich",
-                  "kebab",
-                  "fries",
-                  "donut",
-                ]}
-                indexBy="country"
-                margin={{
-                  top: 50,
-                  right: 130,
-                  bottom: 50,
-                  left: 60,
-                }}
-                padding={0.3}
-                valueScale={{ type: "linear" }}
-                indexScale={{ type: "band", round: true }}
-                colors={{ scheme: "nivo" }}
-                defs={[
-                  {
-                    id: "dots",
-                    type: "patternDots",
-                    background: "inherit",
-                    color: "#38bcb2",
-                    size: 4,
-                    padding: 1,
-                    stagger: true,
-                  },
-                  {
-                    id: "lines",
-                    type: "patternLines",
-                    background: "inherit",
-                    color: "#eed312",
-                    rotation: -45,
-                    lineWidth: 6,
-                    spacing: 10,
-                  },
-                ]}
-                fill={[
-                  {
-                    match: {
-                      id: "fries",
+      <div className="w-full p-5">
+        <h1 className="text-xl font-bold text-center">Bar Chart</h1>
+        <div className="h-96">
+          {dashboardIsLoading && (
+            <div className="w-full h-full items-center justify-center flex">
+              <Loader />
+            </div>
+          )}
+          {dashboard && (
+            <AutoSizer>
+              {({ height, width }) => (
+                <Bar
+                  height={height}
+                  width={width}
+                  data={dashboard.data}
+                  keys={[
+                    "hot dog",
+                    "burger",
+                    "sandwich",
+                    "kebab",
+                    "fries",
+                    "donut",
+                  ]}
+                  indexBy="country"
+                  margin={{
+                    top: 50,
+                    right: 130,
+                    bottom: 50,
+                    left: 60,
+                  }}
+                  padding={0.3}
+                  valueScale={{ type: "linear" }}
+                  indexScale={{ type: "band", round: true }}
+                  colors={{ scheme: "nivo" }}
+                  defs={[
+                    {
+                      id: "dots",
+                      type: "patternDots",
+                      background: "inherit",
+                      color: "#38bcb2",
+                      size: 4,
+                      padding: 1,
+                      stagger: true,
                     },
-                    id: "dots",
-                  },
-                  {
-                    match: {
-                      id: "sandwich",
+                    {
+                      id: "lines",
+                      type: "patternLines",
+                      background: "inherit",
+                      color: "#eed312",
+                      rotation: -45,
+                      lineWidth: 6,
+                      spacing: 10,
                     },
-                    id: "lines",
-                  },
-                ]}
-                borderColor={{
-                  from: "color",
-                  modifiers: [["darker", 1.6]],
-                }}
-                axisTop={null}
-                axisRight={null}
-                axisBottom={{
-                  tickSize: 5,
-                  tickPadding: 5,
-                  tickRotation: 0,
-                  legend: "country",
-                  legendPosition: "middle",
-                  legendOffset: 32,
-                }}
-                axisLeft={{
-                  tickSize: 5,
-                  tickPadding: 5,
-                  tickRotation: 0,
-                  legend: "food",
-                  legendPosition: "middle",
-                  legendOffset: -40,
-                }}
-                labelSkipWidth={12}
-                labelSkipHeight={12}
-                labelTextColor={{
-                  from: "color",
-                  modifiers: [["darker", 1.6]],
-                }}
-                legends={[
-                  {
-                    dataFrom: "keys",
-                    anchor: "bottom-right",
-                    direction: "column",
-                    justify: false,
-                    translateX: 120,
-                    translateY: 0,
-                    itemsSpacing: 2,
-                    itemWidth: 100,
-                    itemHeight: 20,
-                    itemDirection: "left-to-right",
-                    itemOpacity: 0.85,
-                    symbolSize: 20,
-                    effects: [
-                      {
-                        on: "hover",
-                        style: {
-                          itemOpacity: 1,
-                        },
+                  ]}
+                  fill={[
+                    {
+                      match: {
+                        id: "fries",
                       },
-                    ],
-                  },
-                ]}
-                role="application"
-                ariaLabel="Nivo bar chart demo"
-                barAriaLabel={function (e) {
-                  return (
-                    e.id +
-                    ": " +
-                    e.formattedValue +
-                    " in country: " +
-                    e.indexValue
-                  );
-                }}
-              />
-            )}
-          </AutoSizer>
-        )}
+                      id: "dots",
+                    },
+                    {
+                      match: {
+                        id: "sandwich",
+                      },
+                      id: "lines",
+                    },
+                  ]}
+                  borderColor={{
+                    from: "color",
+                    modifiers: [["darker", 1.6]],
+                  }}
+                  axisTop={null}
+                  axisRight={null}
+                  axisBottom={{
+                    tickSize: 5,
+                    tickPadding: 5,
+                    tickRotation: 0,
+                    legend: "country",
+                    legendPosition: "middle",
+                    legendOffset: 32,
+                  }}
+                  axisLeft={{
+                    tickSize: 5,
+                    tickPadding: 5,
+                    tickRotation: 0,
+                    legend: "food",
+                    legendPosition: "middle",
+                    legendOffset: -40,
+                  }}
+                  labelSkipWidth={12}
+                  labelSkipHeight={12}
+                  labelTextColor={{
+                    from: "color",
+                    modifiers: [["darker", 1.6]],
+                  }}
+                  legends={[
+                    {
+                      dataFrom: "keys",
+                      anchor: "bottom-right",
+                      direction: "column",
+                      justify: false,
+                      translateX: 120,
+                      translateY: 0,
+                      itemsSpacing: 2,
+                      itemWidth: 100,
+                      itemHeight: 20,
+                      itemDirection: "left-to-right",
+                      itemOpacity: 0.85,
+                      symbolSize: 20,
+                      effects: [
+                        {
+                          on: "hover",
+                          style: {
+                            itemOpacity: 1,
+                          },
+                        },
+                      ],
+                    },
+                  ]}
+                  role="application"
+                  ariaLabel="Nivo bar chart demo"
+                  barAriaLabel={function (e) {
+                    return (
+                      e.id +
+                      ": " +
+                      e.formattedValue +
+                      " in country: " +
+                      e.indexValue
+                    );
+                  }}
+                />
+              )}
+            </AutoSizer>
+          )}
+        </div>
       </div>
     </Card>
   );
