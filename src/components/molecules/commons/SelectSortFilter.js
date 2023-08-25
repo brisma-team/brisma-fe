518dc53f1a39f965b2d402fb5ec813f297ee179e
dandy <vitrio@noxus.co.id>
2023-08-25 15:45:32 +0700

    [Dandy] Update style component SelectSortFilter

--- src/components/molecules/commons/SelectSortFilter.js
+++ src/components/molecules/commons/SelectSortFilter.js
@@ -2,7 +2,7 @@ import Select from "@atlaskit/select";
 
 const SelectSortFilter = ({ change }) => {
   return (
-    <div className="flex items-center px-2">
+    <div className="flex items-center">
       <div className="text-sm">URUTKAN</div>
       <Select
         className="ml-2"
