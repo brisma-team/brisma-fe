be7b3d6fa4e3699385706e649fc23aa59de2ab0a
dandy <vitrio@noxus.co.id>
2023-08-25 15:49:58 +0700

    [Dandy] Modify classname at page Latar Belakang in PAT

--- src/pages/pat/projects/[id]/latar-belakang-dan-tujuan/index.js
+++ src/pages/pat/projects/[id]/latar-belakang-dan-tujuan/index.js
@@ -159,7 +159,7 @@ const index = () => {
                       );
                     })}
                   </div>
-                  <div className="py-2 bg-none w-full justify-center">
+                  <div className="mt-4 px-8 py-2 bg-none w-full justify-center">
                     <UploadButton
                       text={"Tambah Kliping +"}
                       fileAccept={"image/png, image/gif, image/jpeg"}
