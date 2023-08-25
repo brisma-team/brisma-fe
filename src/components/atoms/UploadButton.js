c6698801073f4cc14b42d32da81aeb2a593bef35
dandy <vitrio@noxus.co.id>
2023-08-25 15:43:25 +0700

    [Dandy] Modify UploadButton component

--- src/components/atoms/UploadButton.js
+++ src/components/atoms/UploadButton.js
@@ -1,6 +1,6 @@
 const UploadButton = ({ text, fileAccept, handleUpload }) => {
   return (
-    <div className="mt-4 w-full px-8">
+    <>
       <label
         htmlFor="fileInput"
         className="cursor-pointer max-w-full bg-neutral-50 hover:bg-neutral-100 font-semibold py-2 px-5 rounded-md border-[1.95px] active:bg-neutral-200"
@@ -14,7 +14,7 @@ const UploadButton = ({ text, fileAccept, handleUpload }) => {
         className="hidden"
         onChange={handleUpload}
       />
-    </div>
+    </>
   );
 };
 export default UploadButton;
