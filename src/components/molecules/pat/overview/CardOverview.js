ec30d65e0b683de04ffd40268f95b17f9ceb3d70
dandy <vitrio@noxus.co.id>
2023-09-20 14:55:37 +0700

    [Dandy] adjusment in page project overview PAT

--- src/components/molecules/pat/overview/CardOverview.js
+++ src/components/molecules/pat/overview/CardOverview.js
@@ -1,12 +1,8 @@
-import { ButtonIcon, Card } from "@/components/atoms";
-import {
-  IconEdit,
-  IconMore,
-  IconSetting,
-  IconSuccess,
-} from "@/components/icons";
-import Link from "next/link";
+import { Card, DivButton } from "@/components/atoms";
+import { IconSuccess } from "@/components/icons";
 import ProgressBar from "@atlaskit/progress-bar";
+import { useRouter } from "next/router";
+import DropdownCard from "./DropdownCard";
 
 const CardBody = ({ title, value, icon }) => {
   return (
@@ -32,10 +28,17 @@ const CardOverview = ({
   addendum,
   href,
 }) => {
+  const router = useRouter();
+
+  const handleClickCard = () => {
+    router.push(href);
+  };
+
   return (
-    <Link
+    <DivButton
+      handleClick={handleClickCard}
       className="m-2 hover:bg-gray-100 hover:rounded-[10px] hover:no-underline"
-      href={href}
+      // href={href}
     >
       <Card>
         <div className="w-full px-4 pb-2">
@@ -43,16 +46,8 @@ const CardOverview = ({
             <div className="text-base font-bold text-atlasian-blue-dark">
               {title}
             </div>
-            <div className="h-6 w-6 flex justify-center items-center bg-white rounded hover:border-atlasian-blue-light border border-transparent">
-              <ButtonIcon
-                color={"blue"}
-                icon={
-                  <div className="flex justify-center items-center p-1">
-                    <IconMore size="medium" />
-                  </div>
-                }
-                handleClick={() => console.log("TEST")}
-              />
+            <div className="w-7">
+              <DropdownCard />
             </div>
           </div>
           <div className="text-sm font-bold">{year}</div>
@@ -81,7 +76,7 @@ const CardOverview = ({
           </div>
         </div>
       </Card>
-    </Link>
+    </DivButton>
   );
 };
 
