9a269d90a938fb6f39637771381f433af64dddf6
dandy <vitrio@noxus.co.id>
2023-10-23 11:14:20 +0700

    [Dandy] bug fixing component DivButton

--- src/components/atoms/DivButton.js
+++ src/components/atoms/DivButton.js
@@ -1,38 +1,39 @@
 import React from "react";
 
 const DivButton = ({
-	className,
-	handleClick,
-	children,
-	onMouseEnter,
-	onMouseLeave,
+  className,
+  handleClick,
+  children,
+  onMouseEnter,
+  onMouseLeave,
+  isDisabled,
 }) => {
-	const handleButtonClick = (e) => {
-		e.stopPropagation();
-		handleClick(e);
-	};
+  const handleButtonClick = (e) => {
+    e.stopPropagation();
+    handleClick(e);
+  };
 
-	if (isDisabled) {
-		return <div className={`${className} cursor-not-allowed`}>{children}</div>;
-	}
+  if (isDisabled) {
+    return <div className={`${className} cursor-not-allowed`}>{children}</div>;
+  }
 
-	return (
-		<div
-			role="button"
-			tabIndex={0}
-			className={className}
-			onClick={handleButtonClick}
-			onMouseEnter={onMouseEnter}
-			onMouseLeave={onMouseLeave}
-			onKeyDown={(event) => {
-				if (event.key === "Enter" || event.key === "Space") {
-					return;
-				}
-			}}
-		>
-			{children}
-		</div>
-	);
+  return (
+    <div
+      role="button"
+      tabIndex={0}
+      className={className}
+      onClick={handleButtonClick}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      onKeyDown={(event) => {
+        if (event.key === "Enter" || event.key === "Space") {
+          return;
+        }
+      }}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default DivButton;
