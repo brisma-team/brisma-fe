efc35ae64e15784d97176c58558ac22f65067e84
Barry Samuel <barry.samuel@noxus.co.id>
2023-10-30 18:55:11 +0700

    feat: (WIP) rework modal aktivasi dashboard

--- src/components/molecules/dashboard/ModalAddDashboard.js
+++ src/components/molecules/dashboard/ModalAddDashboard.js
@@ -1,13 +1,127 @@
 import React, { use, useEffect, useState } from "react";
-import { Modal, TextInput, Select, ButtonIcon, CheckboxField } from "@/components/atoms";
+import { Modal, CloseModal, ButtonField, TextInput, Select, ButtonIcon, CheckboxField } from "@/components/atoms";
+import { IconPlus } from "@/components/icons";
 import Button from "@atlaskit/button";
 import AddCircleIcon from "@atlaskit/icon/glyph/add-circle";
 import Close from "@atlaskit/icon/glyph/editor/close";
 import SendIcon from "@atlaskit/icon/glyph/send";
 import useUkaList from "@/data/dashboard/useUkaList";
 import useRoleList from "@/data/dashboard/useRoleList";
+import { confirmationSwal } from "@/helpers";
+
+const ModalFooter = ({ handleConfirm }) => {
+  return (
+    <div className="w-full flex justify-end -my-1">
+      <div className="rounded w-28 bg-atlasian-green">
+        <ButtonField text="Simpan" handler={handleConfirm} />
+      </div>
+    </div>
+  );
+};
 
 const ModalAddDashboard = ({
+  showModal,
+  setShowModal,
+  data,
+  setData,
+  handleSubmit,
+}) => {
+  const [isPublic, setIsPublic] = useState(false);
+
+  const handleCloseModal = async () => {
+    const confirm = await confirmationSwal(
+      "Apakah Anda ingin menutup modal ini?"
+    );
+    if (!confirm.value) {
+      return;
+    }
+
+    setShowModal(false);
+  };
+
+  const handleIsPublicChange = (e) => {
+    console.log(`VALUE = ${e.target.value}`)
+    setIsPublic(isPublic);
+    console.log(isPublic);
+  }
+
+  return (
+    <Modal
+      showModal={showModal}
+      onClickOutside={CloseModal}
+      positionCenter={true}
+      footer={<ModalFooter handleConfirm={handleSubmit} />}
+    >
+      <div className="w-[40rem] relative">
+        <CloseModal handleCloseModal={handleCloseModal} showModal={showModal} />
+        <div className="mb-4 font-bold text-xl">Tambah Dashboard</div>
+        <div className="max-h-[25rem] overflow-y-scroll">
+          <div className="grid grid-cols-7">
+            <div className="p-3 text-base col-span-2 mb-2">Dashboard ID</div>
+            <div className="p-1 col-span-4 mb-2">
+              <TextInput
+                placeholder="Masukkan Superset ID"
+                onChange={(e) => setData({ ...data, embedId: e.target.value })}
+              />
+            </div>
+            <div className="p-3 text-base col-span-2 mb-2">Nama Dashboard</div>
+            <div className="p-1 col-span-4 mb-2">
+              <TextInput
+                placeholder="Masukkan Nama Dashboard"
+                onChange={(e) => console.log(e.target.value)}
+              />
+            </div>
+            <div className="p-3 text-base col-span-2 mb-2">Ditujukan Kepada</div>
+            <div className="p-1 pt-3 col-span-4 mb-2">
+              <CheckboxField
+                label="Publik"
+                handleChange={handleIsPublicChange}
+              />
+            </div>
+          </div>
+          <div className="flex flex-row justify-center">
+            <div className="grid grid-cols-6 col-span-6 pt-1">
+              <div className="p-1 col-span-3">
+                <Select
+                  // optionValue={ukaMapping}
+                  placeholder="Pilih Uka"
+                  // onChange={(e) => setUkas(e.value)}
+                  isSearchable={true}
+                />
+              </div>
+              <div className="p-1 pl-1 col-span-3">
+                <Select
+                  // optionValue={roleMapping}
+                  placeholder="Pilih Role"
+                  // onChange={(e) => setRoles(e.map((obj) => obj.value))}
+                  isSearchable={true}
+                  isMulti={true}
+                />
+              </div>
+              <div className="text-center p-1 pl-1 col-span-6 pt-3">
+                <ButtonField
+                  iconAfter={
+                    <div className="text-atlasian-purple">
+                      <IconPlus size="medium" />
+                    </div>
+                  }
+                  text={`Tambah UKA dan Role`}
+                  textColor={"purple"}
+                  handler={() => console.log('test')}
+                  className={"button"}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  )
+}
+
+
+
+const ModalAddDashboardOld = ({
   showModal,
   setShowModal,
   data,
@@ -30,6 +144,19 @@ const ModalAddDashboard = ({
   const handleNameChange = (e) => {
     setData({ ...data, name: e.target.value });
   };
+
+  const handleIsPublicChange = (e) => {
+    setIsPublic(!isPublic);
+    if (isPublic === true) {
+      setData(curr => {
+        const {alllowlist, ...rest} = curr;
+        rest.isPublic = true;
+        return rest;
+      });  
+    }
+  }
+
+  console.log(isPublic);
 
   const closeModal = () => {
     setShowModal(false);
@@ -104,7 +231,7 @@ const ModalAddDashboard = ({
               <div className="p-1 pt-3 col-span-5">
                 <CheckboxField
                   label="Publik"
-                  handleChange={() => setIsPublic(!isPublic)}
+                  handleChange={handleIsPublicChange}
                 />
               </div>
 
