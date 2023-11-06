261d4bc466bc4aba3a3bfe7a5115944b8610ac67
dandy <vitrio@noxus.co.id>
2023-11-06 16:11:43 +0700

    [Dandy] add condition props in file useMapaEWP

--- src/data/ewp/konvensional/mapa/useMapaEWP.js
+++ src/data/ewp/konvensional/mapa/useMapaEWP.js
@@ -2,9 +2,26 @@ import withTokenFetcher from "@/fetchers/withTokenFetcher";
 import useSWR from "swr";
 
 const useMapaEWP = (type, params) => {
-  const { id } = params;
+  const {
+    id,
+    sub_aktivitas_kode,
+    sub_aktivitas_name,
+    sub_major_kode,
+    sub_major_name,
+    risk_issue_name,
+    risk_issue_kode,
+    mcr_id,
+    risk_assigned,
+  } = params;
 
-  const path = `${process.env.NEXT_PUBLIC_API_URL_EWP}/ewp/mapa/${type}/${id}`;
+  let query = "";
+  switch (type) {
+    case "penugasan":
+      query = `?sub_aktivitas_kode=${sub_aktivitas_kode}&sub_aktivitas_name=${sub_aktivitas_name}&sub_major_kode=${sub_major_kode}&sub_major_name=${sub_major_name}&risk_issue_name=${risk_issue_name}&risk_issue_kode=${risk_issue_kode}&mcr_id=${mcr_id}&risk_assigned=${risk_assigned}`;
+      break;
+  }
+
+  const path = `${process.env.NEXT_PUBLIC_API_URL_EWP}/ewp/mapa/${type}/${id}${query}`;
   const { data, error, mutate, isLoading } = useSWR(path, withTokenFetcher);
 
   return {
