3a0db9d756122f9a38cc60264997415e0d339642
dandy <vitrio@noxus.co.id>
2023-11-06 16:12:55 +0700

    [Dandy] update schema validation Uker Assessment MAPA

--- src/helpers/schemas/ewp/konvensional/mapa/ukerAssessmentMapaEWPSchema.js
+++ src/helpers/schemas/ewp/konvensional/mapa/ukerAssessmentMapaEWPSchema.js
@@ -7,7 +7,6 @@ const ukerAssessmentMapaEWPSchema = yup
       ref_auditee_orgeh_kode: yup.string().required("Orgeh wajib diisi"),
       ref_auditee_branch_kode: yup.string().required("Branch wajib diisi"),
       tipe_uker: yup.string().required("Tipe UKER wajib diisi"),
-      gross_profit: yup.string().required("Gross Profit wajib diisi"),
     })
   )
   .min(1, "Minimal ada 1 data UKER");
