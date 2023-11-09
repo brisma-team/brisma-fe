bc64eb090e8e9d98110962003072db1594aeb2b2
dandy <vitrio@noxus.co.id>
2023-11-09 14:55:42 +0700

    [Dandy] adjusment component NavigationDocument

--- src/components/molecules/commons/NavigationDocument.js
+++ src/components/molecules/commons/NavigationDocument.js
@@ -3,14 +3,14 @@ import { Badge, DivButton } from "@/components/atoms";
 const NavigationDocument = ({ no, title, count, handleClick, activeIndex }) => {
   return (
     <DivButton
-      className={`text-sm py-1 flex justify-between ${
+      className={`text-sm py-1 px-1.5 flex justify-between ${
         activeIndex === no
           ? `bg-atlasian-blue-light rounded text-white hover:text-white font-semibold`
           : `text-brisma hover:text-brisma`
       }`}
       handleClick={handleClick}
     >
-      <div className="w-full flex px-1.5">
+      <div className="w-full flex">
         <div>{no}.</div>
         <div className="ml-2">{title}</div>
       </div>
