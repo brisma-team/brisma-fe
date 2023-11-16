f4cd8f01baf53113fe0d16c6a55597086b8632ab
dandy <vitrio@noxus.co.id>
2023-11-16 09:19:51 +0700

    [Dandy] bug fixing on component ProfileDetail

--- src/components/molecules/commons/ProfileDetail.js
+++ src/components/molecules/commons/ProfileDetail.js
@@ -6,9 +6,9 @@ const ProfileDetail = () => {
     <div className="flex flex-col items-center justify-center mb-8">
       <AvatarDefaultExample />
       <div className="pt-4 flex flex-col items-center justify-center">
-        <p className="font-bold text-base">{user.data.fullName}</p>
-        <p className="-mt-1 mb-2 text-base">{user.data.pn}</p>
-        <RoleLabel text={user.data.jabatan} />
+        <p className="font-bold text-base">{user?.data?.fullName}</p>
+        <p className="-mt-1 mb-2 text-base">{user?.data?.pn}</p>
+        <RoleLabel text={user?.data?.jabatan} />
       </div>
     </div>
   );
