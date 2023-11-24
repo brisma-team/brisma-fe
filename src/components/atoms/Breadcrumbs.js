c1cc636cde51d13de2ab3fd43d5d6a5dc9549953
dandy <vitrio@noxus.co.id>
2023-11-24 15:34:21 +0700

    [Dandy] update props on component Breadcrumbs

--- src/components/atoms/Breadcrumbs.js
+++ src/components/atoms/Breadcrumbs.js
@@ -1,21 +1,21 @@
 import Link from "next/link";
 
 const Breadcrumbs = ({ data }) => {
-  return (
-    data.length && (
-      <div className="justify-start mb-6 flex text-atlasian-gray-dark">
-        {data.map((v, i) => {
-          return (
-            <div className="text-sm mx-0 flex" key={i}>
-              {i >= 1 && <div className="mx-1">/</div>}
-              <Link className="no-underline hover:no-underline" href={v.path}>
-                {v.name}
-              </Link>
-            </div>
-          );
-        })}
-      </div>
-    )
+  return data.length ? (
+    <div className="justify-start mb-6 flex text-atlasian-gray-dark">
+      {data.map((v, i) => {
+        return (
+          <div className="text-sm mx-0 flex" key={i}>
+            {i >= 1 && <div className="mx-1">/</div>}
+            <Link className="no-underline hover:no-underline" href={v.path}>
+              {v.name}
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  ) : (
+    ""
   );
 };
 
