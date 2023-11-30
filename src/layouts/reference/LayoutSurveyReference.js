575a2b8f3af3337dadb38a50a0107c43d1ec0f69
dandy <vitrio@noxus.co.id>
2023-11-30 15:32:28 +0700

    [Dandy] adjusment layout and add new props on component LayoutSurveyReference

--- src/layouts/reference/LayoutSurveyReference.js
+++ src/layouts/reference/LayoutSurveyReference.js
@@ -6,7 +6,11 @@ import { PatSidebarOverview } from "@/components/molecules/pat";
 import { useEffect, useState } from "react";
 import { useOverview } from "@/data/reference/admin-survey/overview";
 
-const LayoutSurveyReference = ({ children }) => {
+const LayoutSurveyReference = ({
+  overflowY,
+  withoutRightSidebar,
+  children,
+}) => {
   const [data, setData] = useState(null);
   const { overview } = useOverview("count", {});
 
@@ -45,7 +49,13 @@ const LayoutSurveyReference = ({ children }) => {
       <div className="flex max-h-screen overflow-y-hidden">
         <div className="flex-1 mt-16" style={{ marginLeft: "260px" }}>
           <div className="main">
-            <div className="pl-5 relative flex justify-between">{children}</div>
+            <div
+              className={`pl-5 relative ${
+                !withoutRightSidebar && `flex justify-between`
+              } ${overflowY && ` max-h-screen overflow-y-scroll`}`}
+            >
+              {children}
+            </div>
           </div>
         </div>
       </div>
