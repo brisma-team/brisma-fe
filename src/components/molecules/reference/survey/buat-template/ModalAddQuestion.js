a0962c53e0af67c370ac407ee2553988d55422f7
dandy <vitrio@noxus.co.id>
2023-11-30 15:30:43 +0700

    [Dandy] adjusment on component ModalAddQuestion

--- src/components/molecules/reference/survey/buat-template/ModalAddQuestion.js
+++ src/components/molecules/reference/survey/buat-template/ModalAddQuestion.js
@@ -5,29 +5,36 @@ import {
 } from "@/components/molecules/commons";
 import { confirmationSwal } from "@/helpers";
 import { ProgressTracker } from "@atlaskit/progress-tracker";
-import { useState } from "react";
 import QuestionTypeSelect from "../../QuestionTypeSelect";
-// import dynamic from "next/dynamic";
-// const Editor = dynamic(() => import("@/components/atoms/Editor"), {
-//   ssr: false,
-// });
+import dynamic from "next/dynamic";
+const Editor = dynamic(() => import("@/components/atoms/Editor"), {
+  ssr: false,
+});
 
 const ModalHeader = ({
   currentContentStage,
   handleChangeCurrentContentStage,
   handleCloseModal,
   showModal,
+  isUpdateModal,
 }) => {
   const progressItems = [
     {
       id: "step-1",
       label: (
-        <DivButton handleClick={() => handleChangeCurrentContentStage(1)}>
+        <DivButton
+          handleClick={() => handleChangeCurrentContentStage(1)}
+          isDisabled={isUpdateModal}
+        >
           Jenis Pertanyaan
         </DivButton>
       ),
       percentageComplete: currentContentStage > 1 ? 100 : 0,
-      status: currentContentStage === 1 ? "current" : "visited",
+      status: isUpdateModal
+        ? "disabled"
+        : currentContentStage === 1
+        ? "current"
+        : "visited",
     },
     {
       id: "step-2",
@@ -71,16 +78,13 @@ const ModalFooter = ({ handleConfirm }) => {
 const ModalAddQuestion = ({
   showModal,
   data,
+  currentContentStage,
   handleCloseModal,
   handleSubmit,
+  isUpdateModal,
   handleChangePayloadQuestion,
+  handleChangeCurrentContentStage,
 }) => {
-  const [currentContentStage, setCurrentContentStage] = useState(1);
-
-  const handleChangeCurrentContentStage = (index) => {
-    setCurrentContentStage(index);
-  };
-
   const closeModal = async () => {
     const confirm = await confirmationSwal(
       "Apakah Anda ingin menutup modal ini?"
@@ -90,8 +94,14 @@ const ModalAddQuestion = ({
     }
 
     handleCloseModal();
-    setCurrentContentStage(1);
+    handleChangeCurrentContentStage(1);
   };
+
+  // [START] Handler for content Guidelines
+  const handleChange = (value) => {
+    handleChangePayloadQuestion("guideline", value);
+  };
+  // [END] Handler for content Guidelines
 
   return (
     <Modal
@@ -102,46 +112,63 @@ const ModalAddQuestion = ({
           handleChangeCurrentContentStage={handleChangeCurrentContentStage}
           handleCloseModal={closeModal}
           showModal={showModal}
+          isUpdateModal={isUpdateModal}
         />
       }
       footer={<ModalFooter handleConfirm={handleSubmit} />}
     >
-      <div className="w-[53rem] p-2">
-        <CardContentHeaderFooter
-          header={
-            <div className="w-full h-full p-3">
-              <p className="text-base font-semibold">Tambah Pertanyaan</p>
+      {currentContentStage === 1 ? (
+        <div className="w-[53rem] p-2">
+          <CardContentHeaderFooter
+            header={
+              <div className="w-full h-full p-3">
+                <p className="text-base font-semibold">Tambah Pertanyaan</p>
+              </div>
+            }
+          >
+            <div className="w-full p-4">
+              <div className="w-1/2">
+                <FormLabel
+                  form={
+                    <QuestionTypeSelect
+                      placeholder={"Jenis Pertanyaan"}
+                      selectedValue={
+                        data.tipe_pertanyaan_kode
+                          ? {
+                              label: data.tipe_pertanyaan_name,
+                              value: {
+                                kode: data.tipe_pertanyaan_kode,
+                                name: data.tipe_pertanyaan_name,
+                              },
+                            }
+                          : null
+                      }
+                      handleChange={(e) =>
+                        handleChangePayloadQuestion("tipe_pertanyaan", e.value)
+                      }
+                    />
+                  }
+                  title={"Jenis Pertanyaan*"}
+                />
+              </div>
             </div>
-          }
-        >
-          <div className="w-full p-4">
-            <div className="w-1/2">
-              <FormLabel
-                form={
-                  <QuestionTypeSelect
-                    placeholder={"Jenis Pertanyaan"}
-                    selectedValue={
-                      data.tipe_pertanyaan_kode
-                        ? {
-                            label: data.tipe_pertanyaan_name,
-                            value: {
-                              kode: data.tipe_pertanyaan_kode,
-                              name: data.tipe_pertanyaan_name,
-                            },
-                          }
-                        : null
-                    }
-                    handleChange={(e) =>
-                      handleChangePayloadQuestion("tipe_pertanyaan", e.value)
-                    }
-                  />
-                }
-                title={"Jenis Pertanyaan*"}
-              />
-            </div>
+          </CardContentHeaderFooter>
+        </div>
+      ) : (
+        <div className="w-[51rem] px-2">
+          <div className="w-full h-full px-2 pb-3">
+            <p className="text-base font-semibold">Guidelines</p>
           </div>
-        </CardContentHeaderFooter>
-      </div>
+          <div className="ckeditor-modal">
+            <Editor
+              contentData={data.guideline}
+              disabled={false}
+              ready={true}
+              onChange={(value) => handleChange(value)}
+            />
+          </div>
+        </div>
+      )}
     </Modal>
   );
 };
