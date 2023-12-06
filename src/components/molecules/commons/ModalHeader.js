2259310f361e4e735de0e91340fa7992ac0db2cb
dandy <vitrio@noxus.co.id>
2023-12-06 17:40:30 +0700

    [Dandy] add new props on component ModalHeader

--- src/components/molecules/commons/ModalHeader.js
+++ src/components/molecules/commons/ModalHeader.js
@@ -1,28 +1,15 @@
 import { CloseModal } from "@/components/atoms";
-import { useEffect } from "react";
 
 const ModalHeader = ({
+  showModal,
   headerText,
   handleCloseModal,
   title,
   titleClassName,
 }) => {
-  const handleKeyDown = (e) => {
-    if (e.key === "Escape") {
-      return handleCloseModal();
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("keydown", handleKeyDown);
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown);
-    };
-  }, []);
-
   return (
     <div style={{ width: "31rem" }} className="relative">
-      <CloseModal handleCloseModal={handleCloseModal} />
+      <CloseModal handleCloseModal={handleCloseModal} showModal={showModal} />
       <div className="text-3xl font-semibold">{headerText}</div>
       {title && <div className={`w-full -mt-4 ${titleClassName}`}>{title}</div>}
     </div>
