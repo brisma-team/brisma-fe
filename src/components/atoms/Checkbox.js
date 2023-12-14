91b34f87a40107945c6eb6ca134307d3487cfb29
Mohammad Dandy Putra <putradandy619@gmail.com>
2023-12-14 10:29:40 +0700

    [Component Atom] add new props isDisabled on component Checkbox

--- src/components/atoms/Checkbox.js
+++ src/components/atoms/Checkbox.js
@@ -1,12 +1,19 @@
 import { Checkbox } from "@atlaskit/checkbox";
 
-const CheckboxField = ({ isChecked, handleChange, label, className }) => {
+const CheckboxField = ({
+  isChecked,
+  handleChange,
+  label,
+  className,
+  isDisabled,
+}) => {
   return (
     <Checkbox
       isChecked={isChecked}
       onChange={handleChange}
       label={label}
       className={className}
+      isDisabled={isDisabled}
     />
   );
 };
