0b2f48248efab163e3dd61de5f1d145f77c67d41
Mohammad Dandy Putra <putradandy619@gmail.com>
2023-12-19 08:45:26 +0700

    [New feature] add new feature export to excel and export to pdf on modul survey

--- src/helpers/previewPrintDocument.js
+++ src/helpers/previewPrintDocument.js
@@ -1,33 +1,72 @@
-const previewPrintDocument = (elementId) => {
+const previewPrintDocument = (elementId, withoutNewTab) => {
   const contentElement = document.getElementById(elementId);
 
   // Mengecek apakah elemen konten telah ditemukan
   if (contentElement) {
     const contentToPrint = contentElement.innerHTML;
 
-    // Membuat jendela baru untuk mencetak konten
-    const printWindow = window.open("", "_blank");
+    // Style untuk menyembunyikan elemen-elemen header dan footer
+    const hidePrintStyles = `
+      <style>
+        @media print {
+          /* Semua elemen header dan footer diatur menjadi display: none; */
+          body {
+            margin: 5px; /* Menghilangkan margin agar tidak ada ruang putih */
+          }
+          @page {
+            size: auto;  /* Auto memungkinkan kustomisasi ukuran kertas */
+            margin: 5px;  /* Menghilangkan margin untuk halaman yang dicetak */
+          }
+          .print-header,
+          .print-footer {
+            display: none !important;
+          }
+        }
+      </style>
+    `;
 
-    // Menuliskan konten dan gaya ke jendela baru
-    printWindow.document.write(`
+    if (withoutNewTab) {
+      document.write(`
         <html>
           <head>
-            <title>Cetak</title>
+            <title>Kuesioner</title>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
+            ${hidePrintStyles}
           </head>
           <body>
-          <div>
-          ${contentToPrint}
-          </div>
+            <div>
+              ${contentToPrint}
+            </div>
           </body>
         </html>
       `);
 
-    // Menutup dokumen baru setelah mencetak
-    printWindow.document.close();
+      setTimeout(() => {
+        window.print();
+      }, 1000);
+    } else {
+      const printWindow = window.open("", "_blank");
 
-    // Menjalankan fungsi pencetakan pada jendela baru
-    printWindow.print();
+      printWindow.document.write(`
+        <html>
+          <head>
+            <title>Kuesioner</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
+            ${hidePrintStyles}
+          </head>
+          <body>
+            <div>
+              ${contentToPrint}
+            </div>
+          </body>
+        </html>
+      `);
+
+      printWindow.document.close();
+      setTimeout(() => {
+        printWindow.print();
+      }, 1000);
+    }
   } else {
     console.error("Elemen konten tidak ditemukan.");
   }
