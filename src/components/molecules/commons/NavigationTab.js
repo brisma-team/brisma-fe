d5c244376d43693d297448bfaa3fb8535425c712
Mohammad Dandy Putra <putradandy619@gmail.com>
2023-12-23 19:06:46 +0700

    [change props] add props handleConfirmation on component NavigationTab

--- src/components/molecules/commons/NavigationTab.js
+++ src/components/molecules/commons/NavigationTab.js
@@ -1,6 +1,22 @@
 import { DivButton } from "@/components/atoms";
 
-const NavigationTab = ({ items, currentStage, setCurrentStage, width }) => {
+const NavigationTab = ({
+  items,
+  currentStage,
+  setCurrentStage,
+  width,
+  handleConfirmation,
+}) => {
+  const handleChangeCurrentStage = async (index) => {
+    if (handleConfirmation) {
+      const confirm = await handleConfirmation();
+      if (!confirm) {
+        return;
+      }
+    }
+    setCurrentStage(index);
+  };
+
   return items?.length ? (
     <div className={`flex border-b-2 ${width || `w-44`} justify-between mb-4`}>
       {items?.map((v, i) => {
@@ -10,7 +26,7 @@ const NavigationTab = ({ items, currentStage, setCurrentStage, width }) => {
             className={`navbar ${v.idx === currentStage && `navbar-active`} ${
               v.isDisabled && `text-atlasian-gray-light`
             }`}
-            handleClick={() => setCurrentStage(v.idx)}
+            handleClick={async () => await handleChangeCurrentStage(v.idx)}
             isDisabled={v.isDisabled}
           >
             {v.title}
