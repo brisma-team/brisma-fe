1ac338abf68aa693263d98302628eac7dc209e8f
Mohammad Dandy Putra <putradandy619@gmail.com>
2023-12-25 16:01:01 +0700

    [change config] add configuration for redis

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      net: false,
+      dns: false,
+      tls: false,
+      os: false,
+    };
+    return config;
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
