068e9b7a8e1361f8fd75d5bd54b8fbbc245ec956
Mohammad Dandy Putra <putradandy619@gmail.com>
2024-01-09 13:55:22 +0700

    [bug fixed] resolve bug error when download kuesioner

--- src/pages/survey/initiator/overview/[id]/preview/index.js
+++ src/pages/survey/initiator/overview/[id]/preview/index.js
@@ -66,7 +66,7 @@ const index = () => {
         if (payloadKuesioner?.length && is_print) {
           previewPrintDocument("content-doc", true);
         }
-      }, 500);
+      }, 1000);
     }
   }, [is_print, payloadKuesioner]);
 
--- src/pages/survey/responden/overview/[id]/index.js
+++ src/pages/survey/responden/overview/[id]/index.js
@@ -136,7 +136,7 @@ const index = () => {
         if (payloadKuesioner?.length && is_print) {
           previewPrintDocument("content-doc", true);
         }
-      }, 500);
+      }, 1000);
     }
   }, [is_print, payloadKuesioner]);
 
