c9db089b19832070112ab39c06913ad1bd3dffb2
Mohammad Dandy Putra <putradandy619@gmail.com>
2024-01-11 09:11:35 +0700

    [add props] component ErrorValidation

--- src/components/atoms/ErrorValidation.js
+++ src/components/atoms/ErrorValidation.js
@@ -1,5 +1,11 @@
-const ErrorValidation = ({ message }) => {
-  return <span className="text-atlasian-red mt-1 text-xs">{message}</span>;
+const ErrorValidation = ({ message, className }) => {
+  return (
+    <span
+      className={`text-atlasian-red mt-1 text-xs ${className && className}`}
+    >
+      {message}
+    </span>
+  );
 };
 
 export default ErrorValidation;
