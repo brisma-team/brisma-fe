4b5ccb25c7fec55e635d6e0e270c843771154f96
Mohammad Dandy Putra <putradandy619@gmail.com>
2024-01-15 15:29:10 +0700

    [add props] add new props on component SelectSortFilter

--- src/components/molecules/commons/SelectSortFilter.js
+++ src/components/molecules/commons/SelectSortFilter.js
@@ -1,17 +1,21 @@
 import Select from "@atlaskit/select";
 
-const SelectSortFilter = ({ change }) => {
+const SelectSortFilter = ({ change, options }) => {
   return (
     <div className="flex items-center gap-3">
       <Select
         className="w-32"
-        options={[
-          { label: "3", value: 3 },
-          { label: "10", value: 10 },
-          { label: "25", value: 25 },
-          { label: "50", value: 50 },
-          { label: "100", value: 100 },
-        ]}
+        options={
+          options?.length
+            ? options.map((v) => ({ label: v.toString(), value: v }))
+            : [
+                { label: "3", value: 3 },
+                { label: "10", value: 10 },
+                { label: "25", value: 25 },
+                { label: "50", value: 50 },
+                { label: "100", value: 100 },
+              ]
+        }
         isSearchable={false}
         onChange={(e) => change("limit", e.value)}
         placeholder={"Limit"}
