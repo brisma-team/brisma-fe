238eb87706d4f9de5be1b1035d5aaf73ae9deceb
Iqbaalaji <vitrio@noxus.co.id>
2024-03-05 14:26:34 +0700

    [Aji] Fixing merge conflict

--- src/components/molecules/commons/index.js
+++ src/components/molecules/commons/index.js
@@ -72,11 +72,8 @@ import RiskSelect from "./RiskSelect";
 import RiskControlSelect from "./RiskControlSelect";
 import ImageClipping from "./ImageClipping";
 import AttachmentClipping from "./AttachmentClipping";
-<<<<<<< HEAD
 import RetryButton from "./RetryButton";
-=======
 import ButtonSave from "./ButtonSave";
->>>>>>> 6b7cd074b0d0be5a3af342e5ba3461c17995f2bc
 
 export {
   BackButton,
