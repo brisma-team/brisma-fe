01ee2b32c4a90c16d0b0e37f6238d9446f453e12
Iqbaalaji <vitrio@noxus.co.id>
2024-03-07 18:10:10 +0700

    [Aji] Fixing Sidebar EWP Consulting

--- src/components/molecules/ewp/konsulting/SidebarLandingEWPConsulting.js
+++ src/components/molecules/ewp/konsulting/SidebarLandingEWPConsulting.js
@@ -1,13 +1,5 @@
 import { useEffect, useState } from "react";
-import { AvatarGroupField } from "@/components/atoms";
-// import { useAuditorEWP } from "@/data/ewp/konvensional";
-import {
-  ImageEclipseGray,
-  ImageEclipseRed,
-  ImageEclipseYellow,
-  ImageBar,
-  ImageCircleCheck,
-} from "@/helpers/imagesUrl";
+import { ImageBar, ImageCircleCheck } from "@/helpers/imagesUrl";
 import {
   NavigationHeader,
   Section,
@@ -18,8 +10,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useProjectDetail } from "@/data/ewp/konsulting";
-// import { useDispatch } from "react-redux";
-// import { setAuditorInfo } from "@/slices/ewp/auditorInfoEWPSlice";
+import { convertDate } from "@/helpers";
 
 const SidebarLandingEWPConsulting = () => {
   //   const dispatch = useDispatch();
@@ -30,55 +21,58 @@ const SidebarLandingEWPConsulting = () => {
   const [currentPage, setCurrentPage] = useState(0);
   //   const [currentPageSub, setCurrentPageSub] = useState(0);
   const { projectDetail } = useProjectDetail({ id });
+  console.log("projectDetail", projectDetail);
 
   useEffect(() => {
     const projectInfo = projectDetail?.data?.project_info;
     setData({
       projectId: projectInfo?.project_id,
       projectName: projectInfo?.project_name,
-      auditTeam: projectInfo?.info_team_audit,
-      statusName: projectInfo?.status_name,
-      statusApprover: projectInfo?.status_approver
-        ? `On ${projectInfo?.status_approver?.pn}`
-        : `-`,
-      addendum: `Addendum ke-${projectInfo?.number_adendum.toString()}`,
+      statusName: projectInfo?.status_persetujuan_name,
+      createdAt: convertDate(projectInfo?.info_periode_pelaksanaan_start, "D"),
+      initiatorName: projectInfo?.initiator?.nama,
     });
-
-    // dispatch(
-    //   setAuditorInfo({
-    //     kta: projectInfo?.info_team_audit?.kta,
-    //     initiator: projectInfo?.info_team_audit,
-    //   })
-    // );
   }, [projectDetail]);
 
-  const arrAvatars = [];
-
-  const groups = ["ma", "kta", "ata"];
-
-  groups.forEach((group) => {
-    if (data?.auditTeam?.[group]) {
-      const avatars = data?.auditTeam[group].map((member) => ({
-        ...member,
-        borderColor:
-          group === "ma" ? "blue" : group === "kta" ? "red" : "green",
-      }));
-      arrAvatars.push(avatars);
-    }
-  });
-
-  const allAvatars = arrAvatars.flat();
-
   const menu = [
-    { name: "Info", href: `/info` },
-    { name: "Perencanaan", href: `/perencanaan` },
-    { name: "Analisa", href: `/analisa` },
-    { name: "Peluang Peningkatan", href: `/peluang-peningkatan` },
-    { name: "Monitoring", href: `/monitoring` },
-    { name: "Meeting", href: `/meeting` },
+    { name: "Info", href: `/info`, isActive: true },
+    {
+      name: "Perencanaan",
+      href: `/perencanaan`,
+      isActive:
+        projectDetail?.data?.realisasi?.penyusunan_mapa_real_start !== null,
+    },
+    {
+      name: "Analisa",
+      href: `/analisa/lingkup`,
+      isActive:
+        projectDetail?.data?.realisasi?.pelaksanaan_audit_real_start !== null,
+    },
+    {
+      name: "Peluang Peningkatan",
+      href: `/peluang-peningkatan`,
+      isActive:
+        projectDetail?.data?.realisasi?.peluang_peningkatan_real_start !== null,
+    },
+    {
+      name: "Wrap up",
+      href: `/wrap-up`,
+      isActive:
+        projectDetail?.data?.realisasi?.Wrapup_Meeting_real_start !== null,
+    },
+    {
+      name: "Close",
+      href: `/Close`,
+      isActive:
+        projectDetail?.data?.realisasi?.Wrapup_Meeting_real_start !== null,
+    },
   ];
 
-  const subMenu = [{ name: "Log", href: `/log` }];
+  const subMenu = [
+    { name: "Log", href: `/log` },
+    { name: "Dokumen", href: `/dokumen` },
+    { name: "Meeting", href: `/meeting` },
+  ];
 
   useEffect(() => {
     const getCurrentPage = () => {
@@ -141,29 +135,28 @@ const SidebarLandingEWPConsulting = () => {
       <SideNavigation label="project" testId="side-navigation">
         <NavigationHeader>
           <div className="flex flex-col px-5 pt-16 pb-3">
-            <div className="text-base text-brisma font-bold">
-              <p>Project Details</p>
-              <p>{data?.projectId?.toUpperCase()}</p>
+            <div className="text-sm text-brisma font-bold mb-4">
+              <p>{data?.projectName?.toUpperCase()}</p>
             </div>
-            <div className="text-sm text-brisma">{data?.projectName}</div>
-            <AvatarGroupField data={allAvatars} />
-            <div className="flex gap-2 my-0.5 items-center">
-              <div>
-                <Image src={ImageEclipseYellow} alt="" />
+            <div className="mb-2">
+              <div className="text-sm text-brisma font-bold">PIC</div>
+              <div className="text-sm text-brisma">
+                {data?.initiatorName || "N/A"}
               </div>
-              <div>{data?.statusName}</div>
             </div>
-            <div className="flex gap-2 my-0.5 items-center">
-              <div>
-                <Image src={ImageEclipseRed} alt="" />
+            <div className="mb-2">
+              <div className="text-sm text-brisma font-bold">Created At</div>
+              <div className="text-sm text-brisma">
+                {data?.createdAt || "N/A"}
               </div>
-              <div>{data?.statusApprover}</div>
             </div>
-            <div className="flex gap-2 my-0.5 items-center">
-              <div>
-                <Image src={ImageEclipseGray} alt="" />
+            <div className="mb-2">
+              <div className="text-sm text-brisma font-bold">
+                Document Status
               </div>
-              <div>{data?.addendum}</div>
+              <div className="text-sm text-brisma">
+                {data?.statusName || "N/A"}
+              </div>
             </div>
           </div>
         </NavigationHeader>
@@ -180,13 +173,20 @@ const SidebarLandingEWPConsulting = () => {
                         currentPage === i
                           ? `text-atlasian-blue-light`
                           : `text-black`
-                      }  gap-2 items-center hover:no-underline no-underline py-2 px-7`}
+                      }  gap-2 items-center hover:no-underline no-underline py-2 px-7 ${
+                        v.isActive
+                          ? `cursor-pointer`
+                          : `hover:cursor-not-allowed pointer-events-none`
+                      }`}
+                      style={{
+                        pointerEvents: v.isActive ? "auto" : "none",
+                        opacity: v.isActive ? 1 : 0.5,
+                        color: v.isActive ? "inherit" : "gray", // Change 'gray' to your desired color
+                      }}
                     >
                       <div>
                         <Image
-                          src={
-                            v.name === "Dokumen" ? ImageCircleCheck : ImageBar
-                          }
+                          src={v.name === "Close" ? ImageCircleCheck : ImageBar}
                           alt=""
                         />
                       </div>
@@ -209,12 +209,7 @@ const SidebarLandingEWPConsulting = () => {
                       className={`flex text-black gap-2 items-center hover:no-underline no-underline py-2 px-7`}
                     >
                       <div>
-                        <Image
-                          src={
-                            v.name === "Dokumen" ? ImageCircleCheck : ImageBar
-                          }
-                          alt=""
-                        />
+                        <Image src={ImageBar} alt="" />
                       </div>
                       <div className="font-semibold text-base">{v.name}</div>
                     </Link>
